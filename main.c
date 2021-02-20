0c18cb7a2101ac2d539525cd4c882ce8d853c685
Andrew Palmer <atp@sdf.org>
2021-02-20 12:28:10 -0600

    Implement max heapify

--- main.c
+++ main.c
@@ -28,8 +28,41 @@ void arr_print(struct arr *this) {
     printf("]\n");
 }
 
+void _swap(int data[], int i1, int i2) {
+    int tmp = data[i1];
+    data[i1] = data[i2];
+    data[i2] = tmp;
+}
+
+#define L_CHILD(head)   ((((head) + 1) * 2) - 1)
+#define R_CHILD(head)   ((head) + 1)
+
+void _max_heapify(int data[], int head, int count) {
+    if(L_CHILD(head) < count) {
+        _max_heapify(data, L_CHILD(head), count);
+        if(data[L_CHILD(head)] > data[head]) {
+            _swap(data, L_CHILD(head), head);
+            _max_heapify(data, L_CHILD(head), count);
+        }
+    }
+
+    if(R_CHILD(head) < count) {
+        _max_heapify(data, R_CHILD(head), count);
+        if(data[R_CHILD(head)] > data[head]) {
+            _swap(data, R_CHILD(head), head);
+            _max_heapify(data, R_CHILD(head), count);
+        }
+    }
+}
+
+void arr_max_heapify(struct arr *this) {
+    _max_heapify(this->values, 0, this->count);
+}
+
 int main(void) {
     struct arr *arr = arr_create(10, 100);
+    arr_print(arr);
+    arr_max_heapify(arr);
     arr_print(arr);
     arr_free(arr);
 }
