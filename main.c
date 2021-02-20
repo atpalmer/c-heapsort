07134d4cbd1e195a02d91cbec23fed2e74f8a937
Andrew Palmer <atp@sdf.org>
2021-02-20 11:39:45 -0600

    Seed randomizer

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ struct arr {
 };
 
 struct arr *arr_create(int count, int mod) {
+    srand(time(NULL));
     struct arr *new = malloc(sizeof(struct arr) + count * sizeof(int));
     new->count = count;
     for(int i = 0; i < count; ++i) {
